472d362499134819d6c815dc90a207f7dc893d81
Jennifer Vendrell <isc20350326@gmail.com>
2023-09-21 11:43:01 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express') //importando el modulo express
+
+const app = express()
+
+//http://localhost:3000/
+app.get('/', (req, res) => {
+    res.json({msg: 'HELLO GET'})
+})
+
+//http://localhost:3000/
+app.get('/:user/:pass', (req, res) => {
+    const {user, pass} = req.params
+    if (user === 'jenni' && pass === '1234'){
+        res.json({msg: 'Iniciaste sesión con éxito'})
+    }
+    res.json({msg: 'Error en el usuario o contraseña'})
+})
+
+// http://localhost:3000/login?user=jenni&password=1234&email=isc20350326@gmail.com
+
+app.get('/login',(req, res)=> {
+    const {user, password} = req.query
+
+    if(!user || !password){
+        res.status(400).json({msg: 'you need to provide <user> and <password> parameters'})
+    return
+    }
+
+    if (user === 'jenni' && password === '1234'){
+        res.json({msg: 'Iniciaste sesión con éxito'})
+    }
+    res.status(404).json({msg: 'Error en el usuario o contraseña'})
+    return
+})
+
+//http://localhost:3000/
+app.get('/', (req, res) => { //Endpoint
+    res.send("Hello with GET!")
+})
+
+//http://localhost:3000/
+app.post('/', (req, res) => {
+    res.send("Hello with POST!")
+})
+
+//http://localhost:3000/
+app.put('/', (req, res) => {
+    res.json({msg: 'HELLO PUT'})
+})
+
+//http://localhost:3000/
+app.patch('/', (req, res) => {
+    res.json({msg: 'HELLO PATCH'})
+})
+
+//http://localhost:3000/
+app.delete('/', (req, res) => {
+    res.json({msg: 'HELLO DELETE'})
+})
+
+app.listen(3000, () => {console.log('listening on port 3000')})
